83dedc85dbe6f6d8e65cec24229de1ef385d4b40
AlessioLucciola <alessiolucciola98@hotmail.it>
2024-03-13 16:18:41 +0100

    Added tqdm to dataloaders

--- dataloaders/RAVDESS_dataloader.py
+++ dataloaders/RAVDESS_dataloader.py
@@ -1,7 +1,7 @@
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
-from config import RAVDESS_DF_SPLITTING, RAVDESS_CSV, RAVDESS_FILES
-from datasets.RAVDESS_dataset import RAVDESSCustomDataset
+from config import RAVDESS_DF_SPLITTING
+from tqdm import tqdm
 
 class RAVDESSDataLoader(DataLoader):
     def __init__(self, dataset, batch_size):
@@ -15,10 +15,10 @@ class RAVDESSDataLoader(DataLoader):
         
 def get_train_val_dataloaders(dataset, batch_size):
     train_df, val_df = train_test_split(dataset, test_size=RAVDESS_DF_SPLITTING[0])
-    train_loader = RAVDESSDataLoader(train_df, batch_size).get_dataloader()
-    val_loader = RAVDESSDataLoader(val_df, batch_size).get_dataloader()
+    train_loader = tqdm(RAVDESSDataLoader(train_df, batch_size).get_dataloader(), desc="Loading Train Dataloader", leave=False)
+    val_loader = tqdm(RAVDESSDataLoader(val_df, batch_size).get_dataloader(), desc="Loading Validation Dataloader", leave=False)
     return train_loader, val_loader, val_df
 
 def get_test_dataloader(dataset, batch_size):
-    test_loader = RAVDESSDataLoader(dataset, batch_size).get_dataloader()
+    test_loader = tqdm(RAVDESSDataLoader(dataset, batch_size).get_dataloader(), desc="Loading Testing Dataloader", leave=False)
     return test_loader
