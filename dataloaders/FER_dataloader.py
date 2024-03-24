69086a7bc892f5e7d835b267b0868d2cd979aab3
Corsi Danilo <danilo.corsi@outlook.it>
2024-03-24 16:06:15 +0100

    Updated dataloaders

--- dataloaders/FER_dataloader.py
+++ dataloaders/FER_dataloader.py
@@ -34,32 +34,3 @@ class FERDataloader:
     def get_test_dataloader(self):
         test_dataset = FERDataset(data=self.data, split='test', transform=None)
         return DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False)
-
-# def FERDataloader(data_dir, val_size=0.2, batch_size=32, shuffle=True, transformations=None): # TODO: change into a class
-#     '''This is function to load the dataset and returns the dataloaders
-#         Args:
-#             data_dir: path to the dataset
-#             batch_size: batch size for the dataloader
-#             num_workers: number of workers for the dataloader
-#             shuffle: whether to shuffle the dataset or not
-#         Returns:
-#             train_loader: dataloader for the training set
-#             val_loader: dataloader for the validation set
-#             test_loader: dataloader for the test set
-#     '''
-#     train_dataset = FERDataset(
-#         data_dir=data_dir, split='train', transform=transformations)
-#     test_dataset = FERDataset(
-#         data_dir=data_dir, split='test', transform=transformations)
-
-
-#     val_len = int(val_size*len(train_dataset))
-#     train_ds, val_ds = random_split(
-#         train_dataset, [len(train_dataset)-val_len, val_len])
-
-#     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=shuffle)
-#     val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=shuffle)
-#     test_loader = DataLoader(
-#         test_dataset, batch_size=batch_size, shuffle=shuffle)
-
-#     return train_loader, val_loader, test_loader
--- datasets/FER_dataset.py
+++ datasets/FER_dataset.py
@@ -55,7 +55,7 @@ class FERDataset(Dataset):
         self.pix_cols = []  # for keeping track of column names
 
         # add each pixel value as a column
-        # TODO: fix -> PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
+        # # TODO: fix -> PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
         for i in range(pixels_values.shape[1]):
             self.pix_cols.append(f'pixel_{i}')
             
@@ -79,83 +79,3 @@ class FERDataset(Dataset):
             img = self.tensor_transform(img)
 
         return img, img_id
-
-# FERPLUS dataset
-
-
-
-# class FERDataset(Dataset):
-#     def __init__(self, data, split='train', transform=None):
-#         train_tfms, val_tfms = get_transformations()
-#         self.transform = transform
-#         self.split = split
-#         if self.transform is None:
-#             self.transform = train_tfms if split == 'train' else val_tfms
-#         self.tensor_transform = transforms.ToTensor()
-#         self.emotions = FER_emotion_mapping
-#         # read the dataset
-#         if self.split == 'train':
-#             data = data.loc[data.Usage.isin(
-#                 ['Training', 'PublicTest'])]
-#             data.reset_index(drop=True, inplace=True)
-#             data = data.drop('Usage', axis=1)
-#         elif self.split == 'test':
-#             data = data.loc[data.Usage.isin(['PrivateTest'])]
-#             data.reset_index(drop=True, inplace=True)
-#             data = data.drop('Usage', axis=1)
-#         else:
-#             raise ValueError(
-#                 "Invalid split type: must be either train or test")
-
-# #         pixels_values = []  # for storing pixel values
-# #         for pix in data.pixels:
-# #             values = [int(i) for i in pix.split()]
-# #             pixels_values.append(values)
-
-# #         pixels_values = np.array(pixels_values)
-        
-#         pixels_values = [[int(i) for i in pix.split()]
-#                          for pix in data.pixels]   # for storing pixel values
-#         pixels_values = np.array(pixels_values)
-#         # rescaling pixel values
-#         pixels_values = pixels_values/255.0
-#         data.drop(columns=['pixels'], axis=1, inplace=True)
-#         self.pix_cols = []  # for keeping track of column names
-
-#         # add each pixel value as a column
-#         # TODO: fix -> PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
-#         for i in range(pixels_values.shape[1]):
-#             self.pix_cols.append(f'pixel_{i}')
-            
-#             data[f'pixel_{i}'] = pixels_values[:, i]
-
-#         self.df = data
-
-#     def __len__(self):
-#         return len(self.df)
-
-#     def __getitem__(self, idx):
-#         row = self.df.iloc[idx]
-#         img_id = int(row['emotion'])
-#         img = np.copy(row[self.pix_cols].values.reshape(48, 48))
-#         img.setflags(write=True)
-
-#         if self.transform:
-#             img = Image.fromarray(img)
-#             img = self.transform(img)
-#         else:
-#             img = self.tensor_transform(img)
-
-#         return img, img_id
-
-#     def plot_distribution(self, path=None):
-#         '''This function plots the distribution of the dataset'''
-#         plt.figure(figsize=(10, 6))
-#         sns.countplot(x='emotion', data=self.df)
-#         plt.title(
-#             f"Emotion Distribution: {self.split.upper()} Dataset", fontsize=18)
-#         plt.xticks(ticks=range(0, 7), labels=[
-#                    self.emotions[i] for i in range(0, 7)], )
-#         if path:
-#             plt.savefig(path)
-#         plt.show()
