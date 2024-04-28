ec09056978463299e8a9f1b07530cbb3bcae9875
Danilo <corsi.1742375@studenti.uniroma1.it>
2024-04-28 17:24:16 +0200

    Testing models

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DEMO_DIR = "demo"
 RANDOM_SEED = 42
 USE_DML = False
 USE_MPS = False
-USE_WANDB = False
+USE_WANDB = True
 SAVE_RESULTS = True
 SAVE_MODELS = True
 
@@ -31,8 +31,8 @@ PATH_MODEL_TO_RESUME = ""
 RESUME_EPOCH = ""
 
 # Train configurations
-BATCH_SIZE = 64 # Max (for ViT): 512 | Max (for CNN): 32
-N_EPOCHS = 500
+BATCH_SIZE = 64 # Max (for ViT): 512 | Max (for CNN): 64
+N_EPOCHS = 25
 LR = 1e-3
 REG = 1e-3
 DROPOUT_P = 0.2
@@ -74,13 +74,13 @@ VIDEO_METADATA_CSV = os.path.join(VIDEO_DATASET_DIR, VIDEO_DATASET_NAME + "_meta
 VIDEO_METADATA_FRAMES_CSV = os.path.join(VIDEO_DATASET_DIR, VIDEO_DATASET_NAME + "_metadata_frames.csv") # _metadata_frames | _metadata_frames_remapped
 
 # Models configurations
-MODEL_NAME = 'vit-pretrained' # Models: resnet18, resnet34, resnet50, resnet101, densenet121, custom-cnn, vit-pretrained
+MODEL_NAME = 'resnet50' # Models: resnet18, resnet34, resnet50, resnet101, densenet121, custom-cnn, vit-pretrained
 HIDDEN_SIZE = [512, 256, 128]  # Hidden layers configurations
 IMG_SIZE = (224, 224)
 NUM_WORKERS = os.cpu_count() # Number of workers for dataloader, set to 0 if you want to run the code in a single process
 
 # Train / Validation
-OVERLAP_SUBJECTS_FRAMES = False # Overlap the frames of the subjects between train, validation and test if True, False otherwise
+OVERLAP_SUBJECTS_FRAMES = True # Overlap the frames of the subjects between train, validation and test if True, False otherwise
 PRELOAD_FRAMES = True # Preload frames if True, load frames on the fly if False
 APPLY_TRANSFORMATIONS = True # Apply transformations if True, use the original dataset if False
 NORMALIZE = True # Normalize the images if True, use the original images if False
@@ -88,4 +88,4 @@ NORMALIZE = True # Normalize the images if True, use the original images if Fals
 # Test configurations
 USE_VIDEO_FOR_TESTING = True # Use test video or live video if True, use test dataset if False
 USE_LIVE_VIDEO_FOR_TESTING = False # If USE_VIDEO = True, use live video if True, use offline video test file if False
-OFFLINE_VIDEO_FILE = os.path.join(VIDEO_DATASET_DIR, "test_video.mp4") # Offline video file
+OFFLINE_VIDEO_FILE = os.path.join(VIDEO_DATASET_DIR, "test_video_real.mp4") # Offline video file
