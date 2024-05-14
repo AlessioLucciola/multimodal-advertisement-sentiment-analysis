05171401473e3d0cb7d02afde78b0857000da3d4
Danilo <corsi.1742375@studenti.uniroma1.it>
2024-05-14 10:19:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ SAVE_RESULTS = True
 SAVE_MODELS = True
 
 # Dataset configurations
-DATASET_NAME: str = "FER" # RAVDESS | FER | ALL
+DATASET_NAME: str = "RAVDESS" # RAVDESS | FER | ALL
 DF_SPLITTING: List = [0.20, 0.50] #[train/test splitting, test/val splitting]
 LIMIT: Optional[int | str] = None # Limit the number of samples in the dataset in percentage (0.5 means use only 50% of the dataset). Use "None" instead.
 BALANCE_DATASET: bool = True # Balance the dataset if True, use the original dataset if False
@@ -38,7 +38,7 @@ RESUME_EPOCH = 60
 
 # Train configurations
 BATCH_SIZE = 256 # Max (for ViT): 256 | Max (for CNN): 64
-N_EPOCHS = 60
+N_EPOCHS = 30
 LR = 1e-3
 REG = 1e-3
 DROPOUT_P = 0.2
@@ -80,9 +80,9 @@ VIDEO_METADATA_CSV = os.path.join(VIDEO_DATASET_DIR, DATASET_NAME + "_metadata_o
 VIDEO_METADATA_FRAMES_CSV = os.path.join(VIDEO_DATASET_DIR, DATASET_NAME + "_metadata_frames.csv") 
 
 # Models configurations
-MODEL_NAME = 'densenet121' # Models: resnet18, resnet34, resnet50, resnet101, densenet121, custom-cnn, vit-pretrained
+MODEL_NAME = 'vit-pretrained' # Models: resnet18, resnet34, resnet50, resnet101, densenet121, custom-cnn, vit-pretrained
 HIDDEN_SIZE = [512, 256, 128]  # Hidden layers configurations
-IMG_SIZE = (48, 48) # (224, 224) for RAVDESS | (48, 48) for FER
+IMG_SIZE = (224, 224) # (224, 224) for RAVDESS | (48, 48) for FER
 NUM_WORKERS = os.cpu_count() # Number of workers for dataloader, set to 0 if you want to run the code in a single process
 
 # Train / Validation configurations
